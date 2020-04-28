a6497fee49a86c4081d6d7c5d4353fec9d24e1bd
19612 <1961207721@qq.com>
2020-04-27 22:54:00 -0400

    Add cuda version of kmeans. Critical kernels are done.

---
+++ cuda_kmeans.h
@@ -0,0 +1,41 @@
+//
+// Created by 19612 on 2020/4/27.
+//
+
+#ifndef INC_418_FINALPROJECT_KNN_CUDA_KMEANS_H
+#define INC_418_FINALPROJECT_KNN_CUDA_KMEANS_H
+
+#include "knn_header.h"
+#define DEFAULT_K 8
+
+class cudaCluster{
+public:
+	cudaCluster(){
+
+	}
+
+	double *device_attributes;
+	int size;
+	int attributesCount;
+	double *centralPoint;//central points' attributes.
+protected:
+
+private:	
+}
+
+class cudaKmeans{
+public:
+	cudaKmeans(){
+
+	}
+
+	cudaCluster *clusters;
+	int *pointClusterIdx;
+protected:
+
+private:	
+}
+
+cudaKmeans getClusters();
+
+#endif //INC_418_FINALPROJECT_KNN_CUDA_KMEANS_H
