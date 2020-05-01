31e7c532237d7016d15130af0a23c3fee72b12b7
19612 <1961207721@qq.com>
2020-05-01 14:04:36 -0400

    complete version of cuda knn.

--- cuda_knn.cu
+++ cuda_knn.cu
@@ -11,6 +11,9 @@
 #define MAXATTRSIZE 8
 #define TRAIN_SIZE 16
 #define TEST_SIZE 16
+#define BLOCK_DIM 64
+#define MAX_K 32
+
 /*
 ComputeDistance: Compute the distances between test instances and training instances.
 				Save the distance in device_distances
@@ -59,9 +62,60 @@ __global__ void initializeIndex(int *device_index, int trainSize, int testSize){
 		device_index[testOffset * trainSize + trainOffset] = trainOffset;
 	}
 }
+
+__global__ void assignLabel(double *device_distances, int *device_index, int *device_label, int *labels, int k, int testSize){
+	//k * testSize total threads
+	__shared__ int sharedLabel[MAX_K * BLOCK_DIM];
+	__shared__ int sharedSize[MAX_K * BLOCK_DIM];
+	__shared__ double sharedDistance[MAX_K * BLOCK_DIM];
+
+	int labelSize = 0;
+	int testOffset = blockDim.x * BLOCK_DIM + threadIdx.x;
+
+	if(testOffset >= testSize)return;
+
+	//Load data
+	for(int i = 0;i < k;i++){
+		int idx = device_index[testOffset * trainSize + i];
+		int newLabel = device_label[idx];
+		double distance = device_distances[idx];
+
+		//check contain
+		int containFlag = -1;
+		for(int j = 0;j < labelSize;j++){
+			if(sharedLabel[threadIdx.x * k + j] == newLabel){
+				containFlag = j;
+				break;
+			}
+		}
+		if(containFlag != -1){
+			sharedDistance[threadIdx.x * k + containFlag] += distance;
+			sharedDistance[threadIdx.x * k + containFlag] += 1;
+		}else{
+			sharedLabel[threadIdx.x * k + labelSize] = newLabel;
+			sharedDistance[threadIdx.x * k + labelSize] = distance;
+			sharedSize[threadIdx.x * k + labelSize] = 1;
+			labelSize++;
+		}
+	}
+
+	double minDistance = INFINITY;
+	int minlabel = -1;
+	for(int i = 0;i < labelSize;i++){
+		int offset = threadIdx.x * k + i;
+		double averageDistance = sharedDistance[offset] / sharedSize[offset];
+		if(averageDistance < minDistance){
+			minDistance = averageDistance;
+			minlabel = sharedLabel[offset];
+		}
+	}
+
+	labels[testOffset] = minlabel;
+}
+
 int *cuPredict(double *trainAttr, int* trainLabels, int trainSize, 
 	double *testAttr, int testSize, int attrSize, int k){
-	double *device_trainAttr, *device_testAttr, *device_trainLabels, *device_distances;
+	double *device_trainAttr, *device_testAttr, *device_trainLabels, *device_distances, device_testLabels;
 	int *device_index;
 
 	cudaMalloc((void **)&device_trainAttr, sizeof(double) * trainSize * attrSize);
@@ -69,6 +123,7 @@ int *cuPredict(double *trainAttr, int* trainLabels, int trainSize,
 	cudaMalloc((void **)&device_index, sizeof(int) * trainSize * testSize);
 	cudaMalloc((void **)&device_testAttr, sizeof(double) * testSize * attrSize);
 	cudaMalloc((void **)&device_distances, sizeof(double) * trainSize * testSize);
+	cudaMalloc((void **)&device_testLabels, sizeof(int) * testSize);
 
 	cudaMemcpy(device_trainAttr, trainAttr, sizeof(double) * trainSize * attrSize, cudaMemcpyHostToDevice);
 	cudaMemcpy(device_trainLabels, trainLabels, sizeof(int) * trainSize, cudaMemcpyHostToDevice);
@@ -95,29 +150,36 @@ int *cuPredict(double *trainAttr, int* trainLabels, int trainSize,
 		thrust::sort_by_key(keys + i * trainSize, keys + (i + 1) * trainSize, vals + i * trainSize);		
 	}
 
-	double *h_distances = new double[trainSize * testSize];
-	int *h_idx = new int[trainSize * testSize];
+	// double *h_distances = new double[trainSize * testSize];
+	// int *h_idx = new int[trainSize * testSize];
 
-        device_distances = thrust::raw_pointer_cast(keys);
-	cudaMemcpy(h_distances, device_distances, sizeof(double) * trainSize * testSize, cudaMemcpyDeviceToHost);
+    device_distances = thrust::raw_pointer_cast(keys);
+    device_index = thrust::raw_pointer_cast(vals);
+	//cudaMemcpy(h_distances, device_distances, sizeof(double) * trainSize * testSize, cudaMemcpyDeviceToHost);
 
-        for(int i = 0;i < attrSize;i++){
-//		printf("attr: %lf\n", testAttr[i]);
 
-	}
-	for(int i = 0;i < trainSize;i++){
-		printf("%lf\n",h_distances[i]);
-	}	
+	int *retLabel = new int[testSize];
+	assignLabel<<<(testSize + BLOCK_DIM - 1 / BLOCK_DIM), BLOCK_DIM>>>(device_distances, device_index, 
+		device_trainLabels, device_testLabels, testSize);
+
+	cudaMemcpy(retLabels, device_testLabels, sizeof(int) * testSize, cudaMemcpyDeviceToHost);
+
+//         for(int i = 0;i < attrSize;i++){
+// //		printf("attr: %lf\n", testAttr[i]);
+
+// 	}
+// 	for(int i = 0;i < trainSize;i++){
+// 		printf("%lf\n",h_distances[i]);
+// 	}	
 
 	cudaFree(device_distances);
-
+	cudaFree(device_testLabels);
 	cudaFree(device_index);
 	cudaFree(device_trainAttr);
 	cudaFree(device_testAttr);
 	cudaFree(device_trainLabels);
 
-	int *ret = new int[10];
-	return ret;
+	return retLabels;
 	//Get distance
 	//Sort distance
 	//find nearest neighbor
