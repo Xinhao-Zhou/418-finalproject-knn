1f1a0edfb2488808e35192ab5e1667e91a7abda8
Sanshan Guo <sanshang@ghc54.ghc.andrew.cmu.edu>
2020-05-01 01:17:36 -0400

    test version of thrust sort in cuda knn

--- cuda_knn.cu
+++ cuda_knn.cu
@@ -1,12 +1,16 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <driver_functions.h>
-
+#include <thrust/sort.h>
+#include <thrust/functional.h>
+#include <thrust/device_ptr.h>
+#include <thrust/iterator/counting_iterator.h>
+#include <thrust/host_vector.h>
 #include "cuda_knn.h"
 
 #define MAXATTRSIZE 8
-#define TRAIN_SIZE 128
-#define TEST_SIZE 128
+#define TRAIN_SIZE 16
+#define TEST_SIZE 16
 /*
 ComputeDistance: Compute the distances between test instances and training instances.
 				Save the distance in device_distances
@@ -45,7 +49,7 @@ __global__ void kernelComputeDistance(double *trainAttr, double *testAttr,
 }
 
 
-__global__ initializeIndex(int *device_index, int trainSize){
+__global__ void initializeIndex(int *device_index, int trainSize){
 	int testOffset = blockDim.y * blockIdx.y;
 	int trainOffset = blockDim.x * blockIdx.x;
 
@@ -56,9 +60,9 @@ __global__ initializeIndex(int *device_index, int trainSize){
 }
 int *cuPredict(double *trainAttr, int* trainLabels, int trainSize, 
 	double *testAttr, int testSize, int attrSize, int k){
-
 	double *device_trainAttr, *device_testAttr, *device_trainLabels, *device_distances;
 	int *device_index;
+
 	cudaMalloc((void **)&device_trainAttr, sizeof(double) * trainSize * attrSize);
 	cudaMalloc((void **)&device_trainLabels, sizeof(int) * trainSize);
 	cudaMalloc((void **)&device_index, sizeof(int) * trainSize * testSize);
@@ -71,24 +75,44 @@ int *cuPredict(double *trainAttr, int* trainLabels, int trainSize,
 
 	int blockdimY = (testSize + TEST_SIZE - 1) / TEST_SIZE;
 	int blockdimX = (trainSize + TRAIN_SIZE - 1) / TRAIN_SIZE;
-	dim3 gridDim(blockdimX, blockdimY);
-	dim3 blockDim(TRAIN_SIZE, TEST_SIZE); 
+
+	dim3 gridDim(blockdimX, blockdimY, 1);
+	dim3 blockDim(TRAIN_SIZE, TEST_SIZE, 1); 
+
 
 	kernelComputeDistance<<<gridDim, blockDim>>>(device_trainAttr, device_testAttr, 
 		device_distances, trainSize,testSize, attrSize);
 
 	initializeIndex<<<gridDim, blockDim>>>(device_index, trainSize);
-	cudaDeviceSynchronize();	
+	cudaDeviceSynchronize();
+
+        thrust::device_ptr<double> keys(device_distances);
+        thrust::device_ptr<int> vals(device_index);
+	
 	for(int i = 0;i < testSize;i++){
-		thrust::sort_by_key(device_distance + i * trainSize, device_distance + (i + 1) * trainSize, device_index + (i * trainSize));
+//                 printf("here!\n");
+		thrust::sort_by_key(keys + i * trainSize, keys + (i + 1) * trainSize, vals + i * trainSize);		
 	}
 
+	double *h_distances = new double[trainSize * testSize];
+	int *h_idx = new int[trainSize * testSize];
+
+        device_distances = thrust::raw_pointer_cast(keys);
+	cudaMemcpy(h_distances, device_distances, sizeof(double) * trainSize * testSize, cudaMemcpyDeviceToHost);
+
+	for(int i = 0;i < trainSize;i++){
+		printf("%lf\n",h_distances[i]);
+	}	
+
 	cudaFree(device_distances);
+
 	cudaFree(device_index);
 	cudaFree(device_trainAttr);
 	cudaFree(device_testAttr);
 	cudaFree(device_trainLabels);
 
+	int *ret = new int[10];
+	return ret;
 	//Get distance
 	//Sort distance
 	//find nearest neighbor
